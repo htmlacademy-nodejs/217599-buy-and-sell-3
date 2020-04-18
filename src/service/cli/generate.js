6b5351dd840e1cf3bf360a37a4505e02ea95e190
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-04-18 14:39:10 +0300

    Учебный проект: покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -23,53 +23,30 @@ const OfferType = {
   sale: `sale`,
   offer: `offer`
 };
-const TITLES = [
-  `Продам книги Стивена Кинга.`,
-  `Продам новую приставку Sony Playstation 5.`,
-  `Продам отличную подборку фильмов на VHS.`,
-  `Куплю антиквариат.`,
-  `Куплю породистого кота.`,
-  `Продам коллекцию журналов «Огонёк».`,
-  `Отдам в хорошие руки подшивку «Мурзилка».`,
-  `Продам советскую посуду. Почти не разбита.`,
-  `Куплю детские санки.`
-];
-const DESCRIPTIONS = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `Две страницы заляпаны свежим кофе.`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  `Кажется, что это хрупкая вещь.`,
-  `Мой дед не мог её сломать.`,
-  `Кому нужен этот новый телефон, если тут такое...`,
-  `Не пытайтесь торговаться. Цену вещам я знаю.`
-];
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`
-];
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_DESCRIPTIONS_PATH = `./data/descriptions.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const getPictureFileName = (imageNumber) => {
   return `item${imageNumber < 10 ? `0${imageNumber}` : imageNumber}.jpg`;
 };
 
-const generateOffers = (count) => (
+const readContent = async (filePath) => {
+  try {
+    return (await fs.readFile(filePath, `utf8`)).trim().split(`\n`);
+  } catch (err) {
+    console.log(chalk.red(err));
+
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, descriptions, categories) => (
   Array(count).fill({}).map(() => ({
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, MaxCount.category)),
-    description: shuffle(DESCRIPTIONS).slice(1, 5).join(` `),
+    category: shuffle(categories).slice(0, getRandomInt(1, MaxCount.category)),
+    description: shuffle(descriptions).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureCount.min, PictureCount.max)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(Cost.min, Cost.max),
   }))
@@ -86,7 +63,11 @@ module.exports = {
       process.exit(ExitCode.error);
     }
 
-    const content = JSON.stringify(generateOffers(countOffer), null, ` `);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const descriptions = await readContent(FILE_DESCRIPTIONS_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const content = JSON
+      .stringify(generateOffers(countOffer, titles, descriptions, categories), null, ` `);
 
     try {
       await fs.writeFile(FILE_NAME, content);
