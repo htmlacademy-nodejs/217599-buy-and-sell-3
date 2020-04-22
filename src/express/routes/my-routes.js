840120a1cfcc3c8949bc86b66a73844db2b33762
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-04-22 22:18:14 +0300

    Учебный проект: шаблонизируй это

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -7,8 +7,17 @@ const myRouter = new Router();
 myRouter.get(`/`, (req, res) => {
   res.send(`/my`);
 });
+myRouter.get(`/tickets`, (req, res) => {
+  res.render(`my/my-tickets`);
+});
+myRouter.get(`/new`, (req, res) => {
+  res.render(`my/my-new-ticket`);
+});
+myRouter.get(`/edit`, (req, res) => {
+  res.render(`my/my-ticket-edit`);
+});
 myRouter.get(`/comments`, (req, res) => {
-  res.send(`/my/comments`);
+  res.render(`my/comments`);
 });
 
 module.exports = myRouter;
